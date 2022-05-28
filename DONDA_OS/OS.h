2f162d01f67214faf9a92d3db8b1e2f514db10be
nucleotide <13713754853@163.com>
2022-05-29 01:21:36 +0800

    磁盘文件卷done

--- DONDA_OS/OS.h
+++ DONDA_OS/OS.h
@@ -13,7 +13,7 @@
 #define NHINO  128		//共128个Hash链表，提供索引i节点（必须为2的幂）
 #define USERNUM  10		//最多允许10个用户登录
 #define DINODESIZ  32	//每个磁盘i节点所占字节
-#define DINODEBLK  32	//所有磁盘i节点共占32个物理块
+#define DINODEBLK  128	//所有磁盘i节点共占128个物理块
 #define FILEBLK  512	//共有512个目录文件物理块
 #define NICFREE  50		//超级块中空闲块数组的最大块数
 #define NICINOD  50		//超级块中空闲节点的最大块数
@@ -21,13 +21,23 @@
 #define DATASTART (2+DINODEBLK)*BLOCKSIZ //目录、文件区起始地址
 
 #include <string>
+#include<vector>
 
 using namespace std;
 
+//普通磁盘块，放文件
+struct DISK_BLOCK
+{
+	string content;		//文件内容
+	int content_len;		//文件大小（在块中占用了多大的空间）
+};
+
+//文件目录项
 struct SFD_ITEM {
 	string file_name;	//文件名
-	int file_id;		//索引节点编号ID
+	int file_id;				//索引节点编号ID
 };
+
 //磁盘i节点
 struct DISK_BFD_ITEM {		
 	int id;				//文件主标识符
@@ -39,6 +49,7 @@ struct DISK_BFD_ITEM {
 	int link_count;		//文件链接计数
 	int last_visited_time;		//最近一次存取时间
 };
+
 //内存i节点
 struct MEM_BFD_ITEM {
 
@@ -59,6 +70,30 @@ struct MEM_BFD_ITEM {
 	struct MEM_BFD_ITEM* next;	//双向hash链表节点指针--下一个
 	struct MEM_BFD_ITEM* prev;	//双向hash链表节点指针--上一个
 
+};
+
+//超级块
+struct SPUERBLOCK
+{
+	int iNode_num;		//i节点的数量
+	int free_iNode_num;		//空闲i节点数
+	vector<int> free_iNode_id;		//空闲i节点的id数组
+
+	int sfd_item_num;		//目录节点的数量
+	int free_sfd_item_num;		//空闲目录节点数
+	vector<int> free_sfd_item_id;		//空闲目录节点的id数组
+
+	int diskblock_num;		//磁盘块的数量
+	int free_diskblock_num;		//空闲磁盘块数
+	vector<int> free_diskblock_id;		//空闲磁盘块的id数组，用作超级块放入内存的栈
+};
+
+//磁盘文件卷
+struct FILE_SYSTEM {
+	SPUERBLOCK superBlock;		//超级块
+	DISK_BFD_ITEM iNode[DINODEBLK];		//磁盘索引结点区，数量为128块
+	vector<SFD_ITEM> SFD[FILEBLK];		//目录块,数量为512块
+	DISK_BLOCK diskBlock[FILEBLK];		//文件块,数量为512块
 };
 
 #endif
