4f622326a03e7f1c7cb8e4f66a3326ce2aef7e78
nucleotide <13713754853@163.com>
2022-05-29 00:33:24 +0800

    确定磁盘和内存 i节点结构

--- DONDA_OS/OS.h
+++ DONDA_OS/OS.h
@@ -9,7 +9,7 @@
 #define PWDSIZ   12		//口令字
 #define PWDNUM   32		//最多可设32个口令登录
 #define NOFILE  20		//每个用户最多可打开20个文件，即用户打开文件最大次数
-#define NADDR 10		//每个i节点最多指向10块，addr[0]~addr[9]
+#define NADDR 13		//每个i节点最多指向13块，addr[0]~addr[9] 为直接地址，addr[10]为一次间址，addr[11]为二次间址，addr[12]为三次间址
 #define NHINO  128		//共128个Hash链表，提供索引i节点（必须为2的幂）
 #define USERNUM  10		//最多允许10个用户登录
 #define DINODESIZ  32	//每个磁盘i节点所占字节
@@ -24,13 +24,39 @@
 
 using namespace std;
 
-struct SFD_item {
+struct SFD_ITEM {
 	string file_name;	//文件名
 	int file_id;		//索引节点编号ID
 };
 
-struct BFD_item {		//i节点
-	int file_id;
+struct DISK_BFD_ITEM {		//磁盘i节点
+	int id;		//文件主标识符
+	int type;		//文件类型 0--普通 1--目录
+	int auth[8];		//8个用户的存取权限  0--无权限  1--有权限
+	int owner;		//文件创建者id
+	int i_addr[NADDR];		//索引数组（物理地址--磁盘块号）
+	int file_len;		//文件长度
+	int link_count;		//文件链接计数
+	int last_visited_time;		//最近一次存取时间
+};
+struct MEM_BFD_ITEM {		//内存i节点
+
+	int id;		//文件主标识符
+	int type;		//文件类型 0--普通 1--目录
+	int auth[8];		//8个用户的存取权限  0--无权限  1--有权限
+	int owner;		//文件创建者id
+	int i_addr[NADDR];		//索引数组（物理地址--磁盘块号）
+	int file_len;		//文件长度
+	int link_count;		//文件链接计数
+	int last_visited_time;		//最近一次存取时间
+
+	int index_num;		//索引节点编号--hash链表节点号
+	int status_lock;		//0--unlocked，1--locked
+	int status_mod;		//0--unmod，1--mod
+	int shared_count;		//共享计数
+
+	struct MEM_BFD_ITEM* next;		//双向hash链表节点指针--下一个
+	struct MEM_BFD_ITEM* prev;		//双向hash链表节点指针--上一个
 
 };
 
