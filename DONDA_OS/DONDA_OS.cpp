0f725ff4704a95ae442b7065e189a04ecd1ec3a8
柔懿 王 <1559342051@qq.com>
2022-06-02 00:24:09 +0800

    臭了

--- DONDA_OS/DONDA_OS.cpp
+++ DONDA_OS/DONDA_OS.cpp
@@ -7,12 +7,12 @@ USER userList[9];       //所有用户
 int sfd_pointer;		//sfd指针--指向当前的sfd目录
 vector<SFD> sfd_stack;	//目录栈
 
-int main()
+void main()
 { 
     init();
-    login();
-    system("cls");
-    cout << "已成功退出 DONDA_OS 文件系统！";
+    //login();
+    //system("cls");
+    //cout << "已成功退出 DONDA_OS 文件系统！";
 }
 
 
