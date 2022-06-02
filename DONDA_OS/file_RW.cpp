759e918bbe2ac675d1d4df25655b8889ae2bec70
柔懿 王 <1559342051@qq.com>
2022-06-03 03:29:05 +0800

    去

--- DONDA_OS/file_RW.cpp
+++ DONDA_OS/file_RW.cpp
@@ -42,13 +42,10 @@ void writeFile(string fileName)
         }
     }
   
-    if (mem_iNode[iNode_id].status_lock == 0)
-    {
+    if (mem_iNode[iNode_id].status_lock == 0) //上写锁
         mem_iNode[iNode_id].status_lock = 1;
-    }
-    else {
+    else 
         cout << "文件正在被占用！！" << endl;
-    }
     if (iNode_id == -1 || mem_iNode[iNode_id].type == 1) {
         cout << "文件不存在！\n";
         return;
@@ -76,7 +73,7 @@ void tempToDiskBlock(string fileName)
 {
     string content;
     ifstream tempFile;
-    tempFile.open("Temp\\"+fileName+".txt", ios::in);
+    tempFile.open("Temp\\" + fileName + ".txt", ios::in);
     if (tempFile.is_open()) {//新建临时文件成功
         stringstream s;
         s << tempFile.rdbuf();
@@ -101,7 +98,7 @@ void tempToDiskBlock(string fileName)
             }
         }
     }
-    else if(block_num<138&&block_num>=10){
+    else if (block_num < 138 && block_num >= 10) {
         for (int i = 0; i < 10; i++)     //给文件分配磁盘快
         {
             int block_id = AllocateOneBlock();        //分配的磁盘块号
@@ -116,19 +113,19 @@ void tempToDiskBlock(string fileName)
             }
         }
         //一级索引块初始化
-        int index_block1= AllocateOneBlock();
-        for (int i = 0; i < block_num-10; i++)     //给文件分配磁盘快
+        int index_block1 = AllocateOneBlock();
+        for (int i = 10; i < block_num; i++)     //给文件分配磁盘快
         {
             int block_id = AllocateOneBlock();        //分配的磁盘块号
-            fileSystem.diskBlock[index_block1].content+= to_string(block_id)+" ";     //通过文件名找到将分配的磁盘块号写入该文件iNode的索引数组
+            fileSystem.diskBlock[index_block1].content += to_string(block_id) + " ";     //通过文件名找到将分配的磁盘块号写入该文件iNode的索引数组
             fileSystem.diskBlock[index_block1].content_len++;
-            if (i != block_num - 11) {
+            if (i != block_num - 1) {
                 fileSystem.diskBlock[block_id].content = content.substr(i * BLOCKSIZ, BLOCKSIZ);        //每个磁盘块的内容都来自于content对应位置的子串
                 fileSystem.diskBlock[block_id].content_len = BLOCKSIZ;        //每个磁盘块的内容大小都512，因为已经写满了
             }
             else {
                 fileSystem.diskBlock[block_id].content = content.substr(i * BLOCKSIZ);      //最后一个磁盘块的内容都来自于content对应位置的子串
-                fileSystem.diskBlock[block_id].content_len = content.length() - (i * BLOCKSIZ);      //最后一个磁盘块的内容大小不是512
+                fileSystem.diskBlock[block_id].content_len = content.size() - (i * BLOCKSIZ);      //最后一个磁盘块的内容大小不是512
             }
         }
         fileSystem.iNode[findiNodeByName(fileName)].i_addr[10] = index_block1;
